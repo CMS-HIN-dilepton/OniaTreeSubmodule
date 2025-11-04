e966e124c092545f81eab3c8869d730df35c3d57
Florian Damas <florian.damas@cern.ch>
2025-11-04 13:28:52 +0000

    fix centrality tag

--- HiAnalysis/HiOnia/test/hioniaanalyzer_PbPbPrompt_DATA_cfg.py
+++ HiAnalysis/HiOnia/test/hioniaanalyzer_PbPbPrompt_DATA_cfg.py
@@ -133,7 +133,7 @@ print('\n\033[31m~*~ USING NOMINAL CENTRALITY TABLE FOR 2024 PbPb DATA ~*~\033[0
 process.GlobalTag.snapshotTime = cms.string("9999-12-31 23:59:59.000")
 process.GlobalTag.toGet.extend([
     cms.PSet(record = cms.string("HeavyIonRcd"),
-        tag = cms.string("CentralityTable_HFtowers200_DataPbPb_periHYDJETshape_Run3v1302x04_Nominal_Offline"),
+        tag = cms.string("CentralityTable_HFtowers200_DataPbPb_periHYDJETshape_run3v140x01_offline_Nominal"),
         #connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
         connect = cms.string("sqlite_file:/afs/cern.ch/work/n/nsaha/public/for_GO/DBfiles_2024/CentralityTable_HFtowers200_DataPbPb2024_periHYDJETshape_run3v140x01_offline_Nominal.db"),
         label = cms.untracked.string("HFtowers")
