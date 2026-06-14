7e696277ca0fb547532ca02246cd8b484c9da79d
Florian Damas <florian.damas@cern.ch>
2026-06-14 15:44:05 +0000

    only good ZDC runs for MB config

--- HiAnalysis/HiOnia/test/multicrab_HIPhysicsRawPrime_MBZDC_cfg.py
+++ HiAnalysis/HiOnia/test/multicrab_HIPhysicsRawPrime_MBZDC_cfg.py
@@ -28,12 +28,12 @@ config.Data.unitsPerJob = 200
 
 config.Data.allowNonValidInputDataset = True
 config.Data.publication = False
-config.Data.runRange = '374345-375828'
-config.Data.lumiMask = 'https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions23HI/Cert_Collisions2023HI_374288_375823_Muon.json'
+config.Data.runRange = '374804-375746'
+config.Data.lumiMask = 'https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions23HI/Cert_Collisions2023HI_374288_375823_Good_ZDC_Muon.json'
 
 
 config.section_("Site")
-config.Site.storageSite = "T3_CH_CERNBOX"
+config.Site.storageSite = "T2_CH_CERN"
 config.Site.whitelist = ["T2_US_*","T2_CH_CERN","T1_US_*"]
 
 # Multi crab part
@@ -47,7 +47,7 @@ def submit(config):
         print("Failed submitting task: %s" % (cle))
 
 # Submit the jobs: 32 HIRawPrime PDs, ~5.6k files each, average of 140k events/file
-config.Data.outLFNDirBase = '/store/user/fdamas/PbPb2023/'
+config.Data.outLFNDirBase = '/store/group/phys_heavyions/dileptons/Data2023/'
 
 
 for i in range(32):
